3142e007350a9dfb3a62f7fbc132e2f2336b49f2
Cory <ccrowley96@gmail.com>
2019-02-02 05:10:03 -0500

    added db boiler

--- backend/api/index.js
+++ backend/api/index.js
@@ -8,6 +8,7 @@ router.get('/', (req, res, next) => {
     res.send("Welcome to our API");
 });
 
+// Create room
 router.post('/create_room', (req, res, next) => {
     // Create new room
     let room = {
@@ -23,18 +24,20 @@ router.post('/create_room', (req, res, next) => {
     res.status(200).send();
 });
 
+// Delete Room
 router.post('/delete_room/:room_id', (req, res, next) => {
     // Get room to delete
     let room_id = req.params.room_id;
 
     // Filter through data and remove room to be deleted
     data = _.remove(data, (room) => {
-            if(room.roomNumber === room_id)
-                return true; 
+        if(room.roomNumber != room_id){
+            return true; 
+        }
     });
 
     // Print data for debugging
-    console.log(data);
+    console.log('data after deletion', data);
     res.status(200).send();
 });
 
