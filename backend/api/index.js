1dda540215b2caf81d56e6d325de2045d3154b28
Chris Maltais <chris.maltais@hotmail.ca>
2019-02-02 04:45:55 -0500

    Start GET /:room_id/queue

--- backend/api/index.js
+++ backend/api/index.js
@@ -5,6 +5,11 @@ router.get('/', (req, res, next) => {
     res.send("Welcome to our API");
 });
 
+router.post('/')
 
+router.get('/:roomId/queue', async (req, res) => {
+    let roomID = req.params.id;
+    
+});
 
 module.exports = router;
