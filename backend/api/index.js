ebb92ee8419935385eba384705b002729f4a0817
Cory <ccrowley96@gmail.com>
2019-02-02 04:36:06 -0500

    added create room to api

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
+let data = [];
+
 router.get('/', (req, res, next) => {
     res.send("Welcome to our API");
+});
+
+router.post('/create_room', (req, res, next) => {
+    // Create new room
+    let room = {
+        roomNumber: data.length,
+        queue: []   
+    }
+
+    // Add room to data object
+    data.push(room);
+
+    // Print data for debugging
+    console.log(data);
+    res.status(200).send();
+});
+
+router.post('/delete_room', (req, res, next) => {
+
+    // Add room to data object
+    data.push(room);
+
+    // Print data for debugging
+    console.log(data);
+    res.status(200).send();
 });
 
 
