89b8a97671563b784a946eaced028a50c727d071
Cory <ccrowley96@gmail.com>
2019-02-02 13:20:46 -0500

    setting up add song request

--- frontend/src/components/Player/Player.js
+++ frontend/src/components/Player/Player.js
@@ -5,16 +5,76 @@ import "./Player.css";
 
 const spotifyApi = new SpotifyWebApi();
 
-
+//spotify:track:3sTN90bIP2cJ1783ctHykO
 class Player extends Component {
 	constructor(props) {
-		super(props);
+        super(props);
+        
+        this.state = {
+            token: "BQD6fkWO0xU0iGvXr8_U4b6ylKDX-yOi9ajCiXFkLbvHGlxAbnY9_gpomLMIMd7L9jiuFDSDXFjq6dZ3UTsT1AWqTzJFmHu6Sa9Y9Ko6BxQuWhp6bxvRdimMS8I8qdyZEhgSv2Pen01IwYYx4f6DmoFFvU5606KMrXbY4Wm7",
+            deviceId: "",
+            loggedIn: false,
+            error: "",
+            trackName: "Track Name",
+            artistName: "Artist Name",
+            albumName: "Album Name",
+            playing: false,
+            position: 0,
+            duration: 0,
+        };
+
+        this.playerCheckInterval = null;
     }
 
     componentDidMount = () => {
 		if (this.props.access_token !== undefined)
 			spotifyApi.setAccessToken(this.props.access_token);
-	};
+    };
+    
+    checkForPlayer() {
+        const token = this.props.access_token;
+      
+        if (window.Spotify !== null) {
+            clearInterval(this.playerCheckInterval);
+            
+            this.player = new window.Spotify.Player({
+                name: "Decibois Spotify Player",
+                getOAuthToken: cb => { cb(token); },
+            });
+
+            this.createEventHandlers();
+
+            this.player.connect();
+        }
+    }
+
+    handleLogin() {
+        if (this.props.access_token !== "") {
+          // check every second for the player.
+          this.setState({ loggedIn: true });
+          this.playerCheckInterval = setInterval(() => this.checkForPlayer(), 1000);
+        }
+    }
+
+    createEventHandlers() {
+        this.player.on('initialization_error', e => { console.error(e); });
+        this.player.on('authentication_error', e => {
+          console.error(e);
+          this.setState({ loggedIn: false });
+        });
+        this.player.on('account_error', e => { console.error(e); });
+        this.player.on('playback_error', e => { console.error(e); });
+      
+        // Playback status updates
+        this.player.on('player_state_changed', state => { console.log(state); });
+      
+        // Ready
+        this.player.on('ready', data => {
+          let { device_id } = data;
+          console.log("Let the music play on!");
+          this.setState({ deviceId: device_id });
+        });
+      }
 
     render(){
         return (
--- frontend/src/components/Results/Results.js
+++ frontend/src/components/Results/Results.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from 'axios';
 import { setAccessToken, setSearchResults } from "../../actions/dataAction";
 import { connect } from "react-redux";
 import SongTemplate from "../SongTemplate/SongTemplate";
@@ -16,12 +17,25 @@ class Results extends Component {
 		console.log(this.props.results);
 	};
 
+	handleSongClick = (song) => {
+		console.log('here', song)
+		axios.post(`/api/add`)
+		.then(() => {
+			this.setState({results: []});
+		})
+		.catch((err) => console.log(err))
+
+		// console.log(song);
+	}
+
 	render() {
 		return (
 			<div className="results">
-				{this.props.results.map((song, i) => (
-					<SongTemplate key={i} song={song} />
-				))}
+					{this.props.results.map((song, i) => (
+						<div className ="song-item" key ={i} onClick={()=> this.handleSongClick(song)}>
+							<SongTemplate key={i} song={song} />
+						</div>
+					))}
 			</div>
 		);
 	}
