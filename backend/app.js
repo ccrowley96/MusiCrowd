845206bbce66307707fb08d64ca02828bcd26801
Chris Maltais <chris.maltais@hotmail.ca>
2019-02-03 01:33:00 -0500

    Testing path routes

--- backend/app.js
+++ backend/app.js
@@ -178,10 +178,11 @@ app.use("/api", api);
 
 if (process.env.NODE_ENV === 'production') {
 	// Serve any static files
-	app.use(express.static(path.join(__dirname, 'client/build')));
+	app.use(express.static(path.join(__dirname, '/../frontend/build')));
+	console.log('PATH: ', path.join(__dirname, '/../frontend/build'))
 	// Handle React routing, return all requests to React app
 	app.get('*', function(req, res) {
-	  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+	  res.sendFile(path.join(__dirname, './../frontend/build', 'index.html'));
 	});
 }
 
