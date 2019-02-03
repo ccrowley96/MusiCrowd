6ced1d40ea5324676a6d419e6f554d367c63ab86
akinshonibare <akinkunmishonibare@gmail.com>
2019-02-02 22:12:27 -0500

    mvp

--- frontend/src/components/Room/Room.js
+++ frontend/src/components/Room/Room.js
@@ -3,7 +3,6 @@ import Search from "../Search/Search";
 import { setAccessToken } from "../../actions/dataAction";
 import Results from "../Results/Results";
 import Queue from "../Queue/Queue";
-import Player from "../Player/Player";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import axios from "axios";
@@ -15,13 +14,32 @@ class Room extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			roomCode: "",
 			results: [],
 			access_token: "",
 			partyCode: undefined,
-			queue: []
+			queue: [],
+			currentlyPlaying: undefined
 		};
 	}
+
+	componentDidUpdate = () => {
+		this.apiRefresh();
+	};
+
+	apiRefresh = () => {
+		setTimeout(() => {
+			axios
+				.get(`/api/currently_playing/${this.state.partyCode}`)
+				.then(res => {
+					this.setState(
+						{
+							currentlyPlaying: res.data
+						},
+						console.log(res.data)
+					);
+				});
+		}, 500);
+	};
 
 	componentDidMount = () => {
 		var code = this.props.history.location.search.split("=")[1];
@@ -30,14 +48,27 @@ class Room extends Component {
 				partyCode: code
 			},
 			() => {
-				axios.get(`/api/room/${this.state.roomCode}`).then(res => {
+				axios.get(`/api/room/${this.state.partyCode}`).then(res => {
 					this.setState(
 						{
 							access_token: res.data.token
 						},
-						() => this.props.setAccessToken(this.state.access_token)
+						() => {
+							console.log(this.state.access_token);
+							this.props.setAccessToken(this.state.access_token);
+						}
 					);
 				});
+				axios
+					.get(`/api/currently_playing/${this.state.partyCode}`)
+					.then(res => {
+						this.setState(
+							{
+								currentlyPlaying: res.data
+							},
+							this.apiRefresh()
+						);
+					});
 			}
 		);
 	};
@@ -59,13 +90,25 @@ class Room extends Component {
 	};
 
 	render() {
+		var trackImage = "http://media.tumblr.com/tumblr_mf3r1eERKE1qgcb9y.jpg";
+		var trackName = "N/A";
+		var artistName = "N/A";
+
+		if (this.state.currentlyPlaying) {
+			trackImage = this.state.currentlyPlaying.album.images[0].url;
+			trackName = this.state.currentlyPlaying.name;
+			artistName = this.state.currentlyPlaying.album.artists[0].name;
+		}
+
 		return (
 			<Container className="playerContainer">
-				<Player
-					currentlyPlaying={this.state.currentlyPlaying}
-					loadSong={this.loadSong}
-					options={false}
-				/>
+				<div className="room-player">
+					<img src={trackImage} alt="song playing" />
+					<div>
+						<h3>{trackName}</h3>
+						<h5>{artistName}</h5>
+					</div>
+				</div>
 				<div className="room">
 					<div>
 						<Search setResults={this.setResults} />
