f1adf5b7c13eb0fa2d387d982cfc593d8694bd95
Chris Maltais <chris.maltais@hotmail.ca>
2019-02-06 12:55:14 -0500

    🚀 Fix deployment script

--- backend/config/config.js
+++ backend/config/config.js
@@ -5,6 +5,6 @@ if (env === 'development') {
     process.env.HOST="http://localhost:8888";
     process.env.HOST_CLIENT="http://localhost:3000";
     // TO DO: We need to have a seperate Dev DB
-} else if (env === 'prod') {
+} else if (env === 'production') {
     process.env.HOST= "https://www.musicrowd.ca";
 }
