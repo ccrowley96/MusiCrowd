61fd83125457ec21e1b95d428473fed585919fc3
Cory <ccrowley96@gmail.com>
2020-05-23 21:21:39 -0700

    removed http for http://musicrowd.ca domain (purchased again for 2 years)

--- backend/config/config.js
+++ backend/config/config.js
@@ -6,5 +6,5 @@ if (env === 'development') {
     process.env.HOST_CLIENT="http://localhost:3000";
     // TO DO: We need to have a seperate Dev DB
 } else if (env === 'production') {
-    process.env.HOST= "https://www.musicrowd.ca";
+    process.env.HOST= "http://www.musicrowd.ca";
 }
